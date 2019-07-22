b0696afccd91bea350efaa1e6d334f4fdbe9a0fe
Ivan Zhang <izhang@Ivans-MacBook-Pro.local>
2019-07-22 14:34:48 -0700

    react-router-dom package, added two routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Game() {
+  return (
+    <>
+      <h1> Sup. </h1>
+      <Link to="/"> Home </Link>
+    </>
+  );
+}
+
+function Landing() {
+  return (
+    <>
+      <h1> MS Geoguesser</h1>
+      <Link to="/game"> Play </Link>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>MS Geoguesser</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/game" component={Game} />
+        <Route exact path="/" component={Landing} />
+      </div>
+    </Router>
   );
 }
 
