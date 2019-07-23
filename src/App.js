fadae383c1cf131ad5cdcf50b946b8007c37e42f
Ivan Zhang <izhang@Ivans-MacBook-Pro.local>
2019-07-23 11:07:52 -0700

    Switching to google-map-react

--- src/App.js
+++ src/App.js
@@ -1,65 +1,48 @@
-import './style/landing.scss';
-import React from 'react';
-import { compose, withProps } from "recompose"
-import { withGoogleMap, GoogleMap, withScriptjs } from "react-google-maps";
+import "./style/landing.scss";
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import GoogleMapReact from "google-map-react";
 
-const Game = () => {
-  return (
-    <>
-      <MyMapComponent />
-      <h1> Sup. </h1>
-      <Link to="/"> Home </Link>
-    </>
-  );
-}
-
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCwD_7knzyafxUbMx7CYGek02bj9UJcdBE&callback=initMap",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `1000px`, display: 'none' }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-    isMarkerShown: false,
-    defaultZoom: 8,
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) => 
-      <GoogleMap
-      defaultZoom={9}
-      defaultCenter={{ lat: parseFloat(props.userLat), lng: parseFloat(props.userLng) }}
-      >
-      </GoogleMap>
+const Game = () => (
+  <div style={{ height: "100vh", width: "100%" }}>
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: "AIzaSyCwD_7knzyafxUbMx7CYGek02bj9UJcdBE" }}
+      defaultCenter={{
+        lat: 59.95,
+        lng: 30.33
+      }}
+      defaultZoom={0}
+    />
+  </div>
 );
 
 const Landing = () => {
   return (
-      <>
+    <>
       <div className="App">
         <header>
-            <h1>MS Geoguesser</h1>
-          </header>
-          <div className="viewport">
-            <Link className="start-btn" to="/game">
-              Play Geoguesser
-            </Link>
-          </div>
+          <h1>MS Geoguesser</h1>
+        </header>
+        <div className="viewport">
+          <Link className="start-btn" to="/game">
+            Play Geoguesser
+          </Link>
+        </div>
       </div>
       <Link to="/game"> Play </Link>
     </>
   );
-}
+};
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{ height: "100vh", width: "100%" }}>
         <Route exact path="/game" component={Game} />
         <Route exact path="/" component={Landing} />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
