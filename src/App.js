ef0f85170729f6eac2914fb42455f6e4cc1626a3
Stephanie Dover <stdover@microsoft.com>
2019-07-23 13:52:58 -0700

    setting state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import GoogleMapReact from "google-map-react";
 import marker from './assets/placeholder.png';
 
+
+const rad = function(x) {
+  return x * Math.PI / 180;
+};
+
+const getDistance = function(p1, p2) {
+  const R = 6378137; // Earth’s mean radius in meter
+  const dLat = rad(p2.lat - p1.lat);
+  const dLong = rad(p2.lng - p1.lng);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+  return d; // returns the distance in meter
+};
+
+const getScore = (pt1, pt2) => {
+  const distance = getDistance(pt1, pt2) / 1000
+  return Math.round(5000*Math.exp(-distance/1200))+1;
+}
+
 const Marker = () => {
   return (
   <div>
@@ -14,7 +36,12 @@ const Marker = () => {
 
 const Game = () => {
   const [lat, setLat] = useState(0);
+  const [polyline, setPolyline] = useState(0);
   const [lng, setLng] = useState(0);
+  const [score, setScore] = useState(0);
+  const [map, setMap] = useState(0);
+  // const [maps, setMaps] = useState(0);
+  const [origin, setOrigin] = useState(0);
   return (
   <div style={{ height: "400px", width: "400px", position: "absolute", bottom: 0, right: 0 }}>
     <GoogleMapReact
@@ -23,9 +50,24 @@ const Game = () => {
         lat: 47.658427,
         lng: -122.141433
       }}
-      onClick={e =>  {
+      options={{
+        fullscreenControl: false
+      }}
+      onGoogleApiLoaded={(map) => {
+        setOrigin({ lat: 47.658427, lng: -122.141433 });
+        setMap(map.map);
+        // setMaps(map.maps)
+        setPolyline(new map.maps.Polyline({ path: [origin] }));
+      }}
+      onClick={ e =>  {
         setLat(e.lat);
         setLng(e.lng);
+        setScore(getScore({lat: e.lat, lng: e.lng}, origin))
+        if (polyline) {
+          polyline.setPath([{lat: e.lat, lng: e.lng}, origin])
+          polyline.setMap(map);
+        }
+        console.log(score);
       }}
       defaultZoom={0}
       yesIWantToUseGoogleMapApiInternals
