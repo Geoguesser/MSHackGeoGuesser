96e1cefc134e9bfe0900b650833b607b1f215ea5
Stephanie Dover <stdover@microsoft.com>
2019-07-23 11:09:26 -0700

    maps

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import './style/landing.scss';
 import React from 'react';
-import { compose, withProps } from "recompose"
-import { withGoogleMap, GoogleMap, withScriptjs } from "react-google-maps";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Game = () => {
   return (
     <>
-      <MyMapComponent />
-      <h1> Sup. </h1>
+      <div id="map"></div>
       <Link to="/"> Home </Link>
     </>
   );
 }
-
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCwD_7knzyafxUbMx7CYGek02bj9UJcdBE&callback=initMap",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `1000px`, display: 'none' }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-    isMarkerShown: false,
-    defaultZoom: 8,
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) => 
-      <GoogleMap
-      defaultZoom={9}
-      defaultCenter={{ lat: parseFloat(props.userLat), lng: parseFloat(props.userLng) }}
-      >
-      </GoogleMap>
-);
 
 const Landing = () => {
   return (
