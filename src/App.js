b344f8c1d1d9f8c451468f35c277c73f5954025c
Stephanie Dover <stdover@microsoft.com>
2019-07-23 10:00:51 -0700

    adding map for api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style/landing.scss';
 import React from 'react';
-import { GoogleMap, Marker } from "react-google-maps"
+import { compose, withProps } from "recompose"
+import { withGoogleMap, GoogleMap, withScriptjs } from "react-google-maps";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Game = () => {
@@ -13,13 +14,24 @@ const Game = () => {
   );
 }
 
-const MyMapComponent = (props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCwD_7knzyafxUbMx7CYGek02bj9UJcdBE&callback=initMap",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `1000px`, display: 'none' }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+    isMarkerShown: false,
+    defaultZoom: 8,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => 
+      <GoogleMap
+      defaultZoom={9}
+      defaultCenter={{ lat: parseFloat(props.userLat), lng: parseFloat(props.userLng) }}
+      >
+      </GoogleMap>
+);
 
 const Landing = () => {
   return (
