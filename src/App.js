60a0d5bb451954d7467916622a2a7f6eb74f7013
DanceParty <keevandance@nau.edu>
2019-07-25 17:39:50 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -5,27 +5,49 @@ import Landing from "./components/Landing";
 import Score from "./components/Score";
 import Leaderboard from "./components/Leaderboard";
 
-const App = () => {
-  const [totalScore, setTotalScore] = React.useState([]);
-  const [distance, setDistance] = React.useState(0);
-  return (
-    <Router>
-      <Route
-        exact
-        path="/game"
-        component={() => (
-          <Game setTotalScore={setTotalScore} setDistance={setDistance} totalScore={totalScore} />
-        )}
-      />
-      <Route exact path="/" component={Landing} />
-      <Route
-        exact
-        path="/score"
-        component={props => <Score {...props} totalScore={totalScore} />}
-      />
-      <Route exact path="/leaderboard" component={() => <Leaderboard totalScore={totalScore} />} />
-    </Router>
-  );
-};
+class App extends React.Component {
+  state = {
+    totalScore: [],
+    distance: 0
+  };
+
+  setTotalScore = totalScore => {
+    this.setState({ totalScore });
+  };
+
+  setDistance = distance => {
+    this.setState({ distance });
+  };
+
+  render() {
+    const { totalScore, distance } = this.state;
+    return (
+      <Router>
+        <Route
+          exact
+          path="/game"
+          component={() => (
+            <Game
+              setTotalScore={this.setTotalScore}
+              setDistance={this.setDistance}
+              totalScore={totalScore}
+            />
+          )}
+        />
+        <Route exact path="/" component={Landing} />
+        <Route
+          exact
+          path="/score"
+          component={props => <Score {...props} totalScore={totalScore} />}
+        />
+        <Route
+          exact
+          path="/leaderboard"
+          component={() => <Leaderboard totalScore={totalScore} />}
+        />
+      </Router>
+    );
+  }
+}
 
 export default App;
